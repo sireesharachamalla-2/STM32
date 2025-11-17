53f98167bdb7c8af79fd9daef0de2135552203cd
sireesharachamalla-2 <reddy.s2420@gmail.com>
2025-11-17 17:57:25 +0530

    add

---
+++ 32_GPIO_bsrr_Baremetal/Src/main.c
@@ -0,0 +1,20 @@
+#include"stm32l4xx.h"
+
+#define GPIOAEN   (1U << 0)
+#define PIN5      (1U<<5)
+#define LED_PIN   PIN5
+
+int main()
+{
+	RCC->AHB2ENR |= GPIOAEN;
+
+	GPIOA -> MODER |= (1U<<10);
+	GPIOA -> MODER &= ~(1U << 11);
+	while(1)
+	{
+		GPIOA -> BSRR = LED_PIN;
+		for(int i=0;i<1000000;i++);
+		GPIOA -> BSRR = (1<<21);
+		for(int i=0;i<1000000;i++);
+	}
+}
