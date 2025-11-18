318ad599635e30731e7bbbaf7e990fc4ca43298b
RachamallaSireesha <sireesha2.r@gmail.com>
2025-11-18 07:29:55 +0530

    Delete I2C_Test_with_ESP32 directory

--- I2C_Test_with_ESP32/Debug/Src/main.d
+++
@@ -1,7 +0,0 @@
-Src/main.o: ../Src/main.c \
- C:/stm32/I2C_Test_with_ESP32/Drivers/Inc/stm32l476xx.h \
- C:/stm32/I2C_Test_with_ESP32/Drivers/Inc/i2c_master.h \
- C:/stm32/I2C_Test_with_ESP32/Drivers/Inc/stm32l476xx.h
-C:/stm32/I2C_Test_with_ESP32/Drivers/Inc/stm32l476xx.h:
-C:/stm32/I2C_Test_with_ESP32/Drivers/Inc/i2c_master.h:
-C:/stm32/I2C_Test_with_ESP32/Drivers/Inc/stm32l476xx.h:
